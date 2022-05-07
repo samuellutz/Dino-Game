defb089d9e17b4e1309b2ac060015e293afda80c
samuel lutz <samlutz89@gmail.com>
2022-05-07 10:26:26 -0700

    making timeing work

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ const WORLD_HEIGHT = 30
 setPixelToWorldScale()
 window.addEventListener('resize',setPixelToWorldScale)
 
+let lastTime
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+    console.log(delta);
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+window.requestAnimationFrame(update)
+
 function setPixelToWorldScale() {
     let worldToPixelScale
         if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
