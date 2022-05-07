c21a096490c976051ba8da215c21a2d5aec32706
samuel lutz <samlutz89@gmail.com>
2022-05-07 10:59:56 -0700

    helper functions

--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -1,11 +1,12 @@
-export function getCustomProperty(){
+export function getCustomProperty(elem, prop){
+    return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
 
 }
 
-export function setCustomProperty(){
-
+export function setCustomProperty(elem, prop, value){
+    elem.stlye.setProperty(prop, value)
 }
 
-export function incrementCustomProperty() {
-
+export function incrementCustomProperty(elem, prop, inc) {
+    setCustomProperty(elem, prop, getcustomProperty(elem, prop) + inc)
 }
