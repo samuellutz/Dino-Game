8b3552573e4d5b75711f00571f55e38b0f805a88
samuel lutz <samlutz89@gmail.com>
2022-05-08 09:08:52 -0700

    dino runs properly

--- dino.js
+++ dino.js
@@ -1,9 +1,41 @@
-const dinoEl = document.createElement('[data-dino]');
+const dinoEl = document.querySelector('[data-dino]');
+const JUMP_SPEED = .45
+const GRAVITY = .011
+const DINO_FRAME_COUNT = 2
+const FRAME_TIME = 100
+
+
+let isJumping
+let dinoFrame
+let currentFrameTime
 
 export function setupDino(){
-
+ isJumping = false
+ dinoFrame = 0
+ currentFrameTime = 0
 }
 
-export function updateDino(){
+export function updateDino(delta, speedScale){
+    handleRun(delta, speedScale)
+    handleJump()
+}
 
+function handleRun(delta, speedScale) {
+    if (isJumping) {
+      dinoEl.src = `img/dino-stationary.png`
+      return
+    }
+  
+    if (currentFrameTime >= FRAME_TIME) {
+      dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
+      dinoEl.src = `img/dino-run-${dinoFrame}.png`
+      currentFrameTime -= FRAME_TIME
+    }
+    currentFrameTime += delta * speedScale
+  }
+
+function handleJump(delta) {
+    if (!isJumping) return
+
+    
 }
